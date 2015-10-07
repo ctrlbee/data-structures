ffc2bb365d165242d2171069fff0076b556ef3ed
ctrlbee <chris@ctrlbee.com>
2015-10-07 15:50:03 -0700

    Data Structures Sprint - functional objects

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,32 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0; 
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage[counter] = value; 
+    counter++; 
   };
 
   someInstance.dequeue = function() {
+    var results = storage[0]; 
+    delete storage[0]; 
+ 
+    if(counter>0){
+      counter--;   
+    }
+
+    //move each element back 1 place 
+    for(var i=0; i<=counter; i++){
+      storage[i] = storage[i+1];
+    }
+
+    return results; 
   };
 
   someInstance.size = function() {
+    return counter; 
   };
 
   return someInstance;
