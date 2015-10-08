9b2cfd76ef1da1cc7580c76a6229ddabf3c6fac3
Abel <ablwang@ucdavis.edu>
2015-10-07 22:18:09 -0700

    Complete queue for functional-shared, prototypical, and pseudoclassical.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,38 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {}; 
+  obj.storage = {}; 
+  obj.counter = 0; 
+  _.extend(obj, queueMethods); 
+
+  return obj; 
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value) {
+    this.storage[this.counter] = value; 
+    this.counter++; 
+  }, 
+
+  dequeue : function() {
+    var results = this.storage[0]; 
+    delete this.storage[0]; 
+
+    if (this.counter > 0) {
+      this.counter--; 
+    }
+    
+    for (var i = 0; i <= this.counter; i++) {
+      this.storage[i] = this.storage[i + 1]; 
+    }
+
+    return results; 
+  }, 
+
+  size : function() {
+    return this.counter; 
+  }
+};
 
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {}; 
+  this.counter = 0; 
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.counter] = value; 
+  this.counter++; 
+}; 
 
+Queue.prototype.dequeue = function() {
+  var results = this.storage[0]; 
+  delete this.storage[0]; 
+
+  if (this.counter > 0) {
+    this.counter--; 
+  }
+
+  for (var i = 0; i <= this.counter; i++) {
+    this.storage[i] = this.storage[i + 1]; 
+  }
+
+  return results; 
+}; 
+
+Queue.prototype.size = function() {
+  return this.counter; 
+}; 
+
+var pro = new Queue(); 
